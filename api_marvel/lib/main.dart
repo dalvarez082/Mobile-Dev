f25c6d708aea421167bdb9ee2afdde24196910a1
dalvarez082 <diego980910@gmail.com>
2023-09-23 14:01:18 -0500

    Init api marvel

---
+++ api_marvel/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:api_marvel/Pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+            home:Principal(),
+          
+      
+    );
+
+}}
+
